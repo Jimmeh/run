1584fbbbd70d4df29ce01af4168a03566bdfe718
Jimmeh <jimmeh.mcdonald@gmail.com>
2024-03-21 10:46:05 +0000

    Moved api struct to new file

---
+++ cmd/when/forecasts.go
@@ -0,0 +1,56 @@
+package when
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type forecast struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Now struct {
+		Temp      float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+}
+
+type weatherApi struct {
+	key string
+}
+
+func (api weatherApi) getForecast() (forecast, error) {
+	res, err := http.Get(fmt.Sprintf("http://api.weatherapi.com/v1/forecast.json?key=%s&q=M28", api.key))
+	if err != nil {
+		return forecast{}, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return forecast{}, err
+	}
+
+	var f forecast
+	err = json.Unmarshal(body, &f)
+	if err != nil {
+		return forecast{}, err
+	}
+	return f, nil
+}
+
+func NewWeatherApi() (forecastRetriever, error) {
+	key, found := os.LookupEnv("WEATHER_KEY")
+	if !found {
+		return weatherApi{}, fmt.Errorf("WEATHER_KEY not found")
+	}
+	return weatherApi{
+		key: key,
+	}, nil
+}
--- cmd/when/when.go
+++ cmd/when/when.go
@@ -1,74 +1,23 @@
 package when
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
 )
 
-type forecast struct {
-	Location struct {
-		Name    string `json:"name"`
-		Country string `json:"country"`
-	} `json:"location"`
-	Now struct {
-		Temp      float64 `json:"temp_c"`
-		Condition struct {
-			Text string `json:"text"`
-		} `json:"condition"`
-	} `json:"current"`
-}
-
-type weatherApi struct {
-	key string
-}
-
-func (api *weatherApi) getForecast() (forecast, error) {
-	res, err := http.Get(fmt.Sprintf("http://api.weatherapi.com/v1/forecast.json?key=%s&q=M28", api.key))
-	if err != nil {
-		return forecast{}, err
-	}
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return forecast{}, err
-	}
-
-	var f forecast
-	err = json.Unmarshal(body, &f)
-	if err != nil {
-		return forecast{}, err
-	}
-	return f, nil
-}
-
-func NewWeatherApi() (ForecastRetriever, error) {
-	key, found := os.LookupEnv("WEATHER_KEY")
-	if !found {
-		return &weatherApi{}, fmt.Errorf("WEATHER_KEY not found")
-	}
-	return &weatherApi{
-		key: key,
-	}, nil
-}
-
-type ForecastRetriever interface {
+type forecastRetriever interface {
 	getForecast() (forecast, error)
 }
 
-func NewWhenCommand(forecastRetriever ForecastRetriever) Command {
-	return Command{forecastRetriever: forecastRetriever}
+func NewWhenCommand(forecasts forecastRetriever) command {
+	return command{forecasts: forecasts}
 }
 
-type Command struct {
-	forecastRetriever ForecastRetriever
+type command struct {
+	forecasts forecastRetriever
 }
 
-func (cmd *Command) Run() error {
-	forecast, err := cmd.forecastRetriever.getForecast()
+func (cmd command) Run() error {
+	forecast, err := cmd.forecasts.getForecast()
 	if err != nil {
 		return err
 	}
